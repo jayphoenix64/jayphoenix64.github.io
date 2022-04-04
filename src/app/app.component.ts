43d8993eb45f676b5977bc09a19cd6b864e15af6
jayphoenix64 <phoenix.janus@gmail.com>
2022-04-04 19:34:34 +0200

    new deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+
+  public welcome = 'Thanks for clicking the button! And Yvi is great.';
+  public show = false;
+
+  public showWelcomeMessage() {
+    if (this.show) {
+      this.show = false
+    } else {
+      this.show = true
+    }
+  }
 }
