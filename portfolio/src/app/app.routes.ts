e17923a1f94412e4e2a867239045a6d289669ec0
jayphoenix64 <phoenix.janus@gmail.com>
2023-10-27 11:03:22 +0200

    made new workspace

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const appRoutes: Route[] = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+];
