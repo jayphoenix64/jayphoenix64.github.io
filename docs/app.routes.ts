26d14595aee1a4256e0132cfdb660b9ddfd2b398
jayphoenix64 <phoenix.janus@gmail.com>
2025-07-14 18:13:21 +0200

    new deploy

--- docs/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MaintenanceComponent } from './maintenance/maintenance.component';
-
-export const appRoutes: Route[] = [
-  { path: 'home', component: HomeComponent },
-  { path: 'maintenance', component: MaintenanceComponent },
-  { path: '', component: MaintenanceComponent },
-];
